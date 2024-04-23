a4faf66f55b3fe021456d53cf9407b6fe5db0411
suer-tech <suer2505@gmail.com>
2024-04-23 15:43:05 +0300

    Add create_connection in main.py

--- uploader/main.py
+++ uploader/main.py
@@ -1,2 +1,15 @@
+import psycopg2
+
+
+def create_connection():
+    return psycopg2.connect(
+        dbname="db",
+        user="user",
+        password="password",
+        host="localhost",
+        port="5432",
+    )
+
+
 if __name__ == "__main__":
     xml_file_path = "elektronika_products_20240423_114945.xml"
